6b475072e7fe731700e9fa43dc1b641ef34f92fc
Rakhshan <rakhshancoder@gmail.com>
2024-09-15 01:28:49 +0530

    baseUrl updated

--- harvestify-frontend/src/util/baseUrl.js
+++ harvestify-frontend/src/util/baseUrl.js
@@ -1,3 +1,1 @@
-// export const baseUrl = 'http://localhost:5000';
-// export const baseUrl = 'https://harvestify-backend.onrender.com';
-export const baseUrl = 'https://harvestify-3wwe.onrender.com';
+export const baseUrl = import.meta.env.VITE_BACKEND_URL;
